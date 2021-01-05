6ce7259e3f9c4742c0b26a22f38afd79429718d0
Aleksander Wons <aleksander.wons@gmail.com>
2021-01-05 17:10:01 +0100

    Rename folders

--- user-pool/bin/user-pool.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { UserPoolStack } from '../lib/user-pool-stack';
-
-require('dotenv').config();
-
-const app = new cdk.App();
-new UserPoolStack(app, 'CBE-E002-UserPoolStack');
